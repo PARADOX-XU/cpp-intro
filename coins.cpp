70c4a808e6151cbcb7501d879533162f45b5bf78
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2021-10-31 00:43:34 +0800

    Create coins.cpp

---
+++ coins.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int dollars, cents, quarters, dimes, nickels, pennies, money;
+
+
+	cout<<"Please enter the amount of money to convert:"<<endl;
+
+	cout<<"# of dollars: ";
+	cin>>dollars;
+
+	cout<<"# of cents: ";
+	cin>>cents;
+
+
+	money = dollars*100 + cents;
+  
+	quarters = money / 25;
+	money = money % 25;
+  
+	dimes = money / 10;
+	money = money % 10;
+
+	nickels = money / 5;
+	money = money % 5;
+
+	pennies = money;
+
+	cout<<"The coins are "<<quarters<<" quarters, "<<dimes<<" dimes, "<<nickels<<" nickels and "<<pennies<<" pennies";
+
+	return 0;
+
+}
