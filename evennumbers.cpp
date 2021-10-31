a14e261b7fc1c8763a715cceef2a5bf47e0a8625
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2021-10-31 18:58:30 +0800

    Create evennumbers.cpp

---
+++ evennumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int number;
+	int i = 0;
+	
+	cout<<"Please enter a positive integer: ";
+	cin>>number;
+	
+	while(i < number){
+		cout<<(i+1)*2<<endl;
+		i++;
+	}
+	return 0;
+}
